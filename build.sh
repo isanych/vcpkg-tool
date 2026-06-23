c87498a6436357248ef70668f2157578928b391c
Igor Kostenko <ikostenko@perforce.com>
2026-06-23 10:47:16 +0100

    static vcpkg on linux

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 d=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 export CC=`which gcc`
 export CXX=`which g++`
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/vcpkg/installed/x64l/lib"
-cmake "$d" -Wno-dev -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64l "$@"
+cmake "$d" -Wno-dev "$@"
 cmake --build .
--- build_ninja_release.sh
+++ build_ninja_release.sh
@@ -3,6 +3,5 @@
 d=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 export CC=`which gcc`
 export CXX=`which g++`
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/vcpkg/installed/x64l/lib"
-cmake "$d" -Wno-dev -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64l -GNinja -DCMAKE_BUILD_TYPE=Release "$@"
+cmake "$d" -Wno-dev -GNinja -DCMAKE_BUILD_TYPE=Release "$@"
 ninja
